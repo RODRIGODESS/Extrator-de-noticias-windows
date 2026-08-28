8dccbc27b5b80b8b02d0442be2e39c737de15fcf
RODRIGODESS <diguinhodess@gmail.com>
2026-08-28 14:37:32 -0300

    Expõe IPC seguro para credenciais de assinante

--- preload.js
+++ preload.js
@@ -6,5 +6,8 @@ contextBridge.exposeInMainWorld('extratorAPI', {
   abrirHistorico: () => ipcRenderer.invoke('abrir-historico'),
   abrirPasta: () => ipcRenderer.invoke('abrir-pasta'),
   obterEstado: () => ipcRenderer.invoke('obter-estado'),
+  obterCredenciaisAssinante: (provedor) => ipcRenderer.invoke('obter-credenciais-assinante', provedor),
+  salvarCredenciaisAssinante: (provedor, dados) => ipcRenderer.invoke('salvar-credenciais-assinante', provedor, dados),
+  apagarCredenciaisAssinante: (provedor) => ipcRenderer.invoke('apagar-credenciais-assinante', provedor),
   onStatus: (callback) => ipcRenderer.on('extracao-status', (_event, mensagem) => callback(mensagem))
 });

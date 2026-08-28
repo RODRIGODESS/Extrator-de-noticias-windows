a4e950af608362633cd059eaa8d31b0e01a01520
RODRIGODESS <diguinhodess@gmail.com>
2026-08-28 11:20:48 -0300

    Aponta runtime compatível para V1.25.16

--- engine/extrator-materia-v1.25.10-runtime.js
+++ engine/extrator-materia-v1.25.10-runtime.js
@@ -1,3 +1,3 @@
 // Compatibilidade: o restante do aplicativo continua apontando para este arquivo.
 // A implementação ativa fica isolada na versão mais recente, sem alterar o main.js.
-module.exports = require('./extrator-materia-v1.25.15-runtime.js');
+module.exports = require('./extrator-materia-v1.25.16-runtime.js');
